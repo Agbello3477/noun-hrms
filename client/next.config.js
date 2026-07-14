415a5a33fd939f05b768ebba31df920d43ce739f
Abdulgaffar Bello <abdulgaffarbello@Abdulgaffars-MacBook-Pro.local>
2026-07-14 19:59:39 +0100

    chore: enable Next.js static export for free Firebase Hosting

--- client/next.config.js
+++ client/next.config.js
@@ -1,24 +1,13 @@
 const nextConfig = {
-    output: process.env.NEXT_STANDALONE === "true" ? "standalone" : undefined,
+    output: "export",
+    images: {
+        unoptimized: true
+    },
     webpack: (config, { dev }) => {
         if (dev) {
             config.cache = false;
         }
         return config;
-    },
-    async redirects() {
-        return [
-            {
-                source: '/login',
-                destination: '/?login=true',
-                permanent: true,
-            },
-            {
-                source: '/register',
-                destination: '/?register=true',
-                permanent: true,
-            },
-        ];
     },
 };
 
